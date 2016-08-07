b6a41470aa7a8ee2b7edd50a237255bf73eb1d34
jftg <jftg29@gmail.com>
2016-08-07 02:20:28 -0500

    ...

--- WEBAPP/Views/registro.marca.php
+++ WEBAPP/Views/registro.marca.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<h1>Guardar Marca</h1>
+	<!-- Formulario: Guardar Marca de producto -->
 	<form action="../../Controller/marca.controller.php" method="POST">
-		<label>Codigo de la marca</label>
-		<input type="text" name="txt_codmarca"/></br>
-		<label>Nombre de la marca</label>
-		<input type="text" name="txt_nommarca"/></br>
-		<label>Logo de la marca</label>
-		<input type="file" name="img_logomarca"></br>
+		<h1>Guardar Marca</h1>
+		<label>Codigo de marca</label>
+		<input type="text" name="txt_codmarca"/>
+		<label>Nombre de marca</label>
+		<input type="text" name="txt_nommarca"/>
+		<label>Logo de marca</label>
+		<input type="file" name="img_logomarca">
 		<button name="accion" value="create">Guardar</button>
-		<?php echo @$_REQUEST["$mensaje"];
-		?>
+		<?php echo @$_REQUEST["$mensaje"]; ?>
 	</form>
-
-</body>
-</html>

fbc6eed05b47be007eaa9b10ec0f87a4c6368073
jftg <jftg29@gmail.com>
2016-08-18 22:01:00 -0500

    ---

--- WEBAPP/Controller/validar.log.php
+++ WEBAPP/Controller/validar.log.php
@@ -6,10 +6,6 @@
 
     if (isset($_POST["sesion_vigilante"])) {
 
-        $usuario= htmlentities(addslashes($_POST["nombre"]));
-        $contrasena= htmlentities(addslashes($_POST["contrasena"]));
-        $name = str_replace("usu", "123456789", $usuario);
-        $pass = str_replace("pas", "123456789", $contrasena);
 
           try {
        login::valida($name,$pass);
